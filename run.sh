4d44fae649de571c6a5b10d808816c9087edbcfb
krrish-jindal <krrishjindal2002@gmail.com>
2024-02-03 20:11:34 +0530

    .sh file fix support both gpu & non gpu

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 xhost +local:root
 
-docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
--it --rm --net=host --ipc=host \
---env="QT_X11_NO_MITSHM=1"  \
---env="DISPLAY"  \
-my_image /bin/bash -c "cd src  && exec bash "
+# Check if NVIDIA Container Toolkit is installed
+if command -v nvidia-container-toolkit &> /dev/null; then
+    # If available, use GPU resources
+    docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+    -it --rm --net=host --ipc=host \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="DISPLAY" \
+    my_image /bin/bash -c "cd src  && exec bash "
+else
+    # If not available, run without GPU support
+    docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+    -it --rm --net=host --ipc=host \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="DISPLAY" \
+    my_image /bin/bash -c "cd src  && exec bash "
+fi
